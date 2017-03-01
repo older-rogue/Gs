97d997a9237f3df3826f9f28bf3295fbbbbff468
older-rogue <1058083107@qq.com>
2017-03-01 18:18:02 +0800

    first

---
+++ app/src/main/java/com/xzll/gs/view/IMainActivity.java
@@ -0,0 +1,8 @@
+package com.xzll.gs.view;
+
+/**
+ * Created by Administrator on 2017/3/1.
+ */
+
+public interface IMainActivity {
+}
---
+++ app/src/main/java/com/xzll/gs/view/impl/MainActivity.java
@@ -0,0 +1,15 @@
+package com.xzll.gs.view.impl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.xzll.gs.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
